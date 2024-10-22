83882fd261f3927fec8099492f9466f48a2b2fa5
t3nshi9 <t3nshi9@gmail.com>
2024-10-22 15:48:10 +0100

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-//import '/style.css';
+import '/style.css';
 import * as THREE from 'three';
 //import { OrbitControls } from 'three/examples/controls/OrbitControls';
 //import { GLTFLoader } from 'three/examples/loaders/GLTFLoader';
